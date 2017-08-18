a89cf1890a90d78f7ba8c4b2bd7b2d1673061e7c
mploskon <mploskon@lbl.gov>
2017-08-17 17:58:51 -0700

    path fixes

---
+++ test/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd $(dirname $BASH_SOURCE)
+BT_config=./test.cfg
+source ../bt.sh "$@" --download --build
+
+function build()
+{
+	cd ${BT_src_dir}
+    if [ -z ${cgalDIR} ]; then
+        ./configure --prefix=${BT_install_dir}
+    else
+            echo "[i] building using cgal at ${cgalDIR}"
+        ./configure --prefix=${BT_install_dir} --enable-cgal --with-cgaldir=${cgalDIR} LDFLAGS=-Wl,-rpath,${boostDIR}/lib CXXFLAGS=-I${boostDIR}/include CPPFLAGS=-I${boostDIR}/include
+    fi
+    [ ${BT_clean} ] && make clean
+    make -j $(n_cores)
+    make install
+}
+
+run_build
