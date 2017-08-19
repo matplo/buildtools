e3437c177f10eb70e0ea7050e75544b5f096a83e
matplo <ploskon@gmail.com>
2017-08-18 20:07:49 -0700

    comments; added now and config_dir

--- bt.sh
+++ bt.sh
@@ -5,7 +5,7 @@
 # within build() rely on $BT variables
 # the simplest implementation
 # BT_config=./tmp.cfg
-# touch BT_config
+# touch $BT_config
 # source bt.sh "$@"
 # function build()
 # {
@@ -14,7 +14,7 @@
 # exec_build_tool
 
 BT_global_args="$@"
-BT_built_in_options="build build_type clean cleanup config debug download dry force help install_dir module module_dir module_paths modules name rebuild src_dir verbose version working_dir"
+BT_built_in_options="build build_type clean cleanup config config_dir debug download dry force help install_dir module module_dir module_paths modules name now rebuild src_dir verbose version working_dir"
 
 function abspath()
 {
